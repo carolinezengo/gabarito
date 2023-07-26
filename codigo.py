6da6bd1899ce82d31f6ae0078e2610e72dd29797
carolinezengo <carolinezengo@gmail.com>
2023-07-26 16:22:53 -0300

    Importação pyautogui

---
+++ codigo.py
@@ -0,0 +1,17 @@
+#Passo a passo
+#Passo1: Entrar no sitema da empresa 
+#"https://dlp.hashtagtreinamentos.com/python/intensivao/login")
+import pyautogui
+#pyautogui.write -> escrever um texto
+#pyautogui.press -> aperta 1 tecla
+#pyautogui.click -> clicar em algum lugar tela
+#abrir p navegador
+pyautogui.press("win")
+pyautogui.write("chrome")
+pyautogui.press("enter")
+#Passo2:Fazer Login
+#Passo3: Importar a base Produto
+#Passo 4: Cadastrar um produto
+#Passo5: Repetir o processo de cadastro ate o fim 
+
+
