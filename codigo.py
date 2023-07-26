84120e7119091d46cc99414bae05683bf60a6f8a
carolinezengo <carolinezengo@gmail.com>
2023-07-26 16:34:40 -0300

    Importação do Time

--- codigo.py
+++ codigo.py
@@ -5,10 +5,17 @@ import pyautogui
 #pyautogui.write -> escrever um texto
 #pyautogui.press -> aperta 1 tecla
 #pyautogui.click -> clicar em algum lugar tela
+#pyautogui.hotkey -> junçao de teclas 
+pyautogui.PAUSE = 0.5
 #abrir p navegador
 pyautogui.press("win")
 pyautogui.write("chrome")
 pyautogui.press("enter")
+#entrar no link
+pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
+pyautogui.press("enter")
+
+
 #Passo2:Fazer Login
 #Passo3: Importar a base Produto
 #Passo 4: Cadastrar um produto
